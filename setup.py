4d87d3d297cd400d3a211b2c644f2ffef4b8970d
pmikus <pmikus@cisco.com>
2016-02-15 13:17:45 +0100

    v2.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'sys-info-suite',
-  packages = ['sys-info-suite'],
-  version = '2.0.1',
+  name = 'sysinfosuite',
+  packages = ['sysinfosuite', 'sysinfosuite.outputs'],
+  version = '2.0.2',
   description = 'System information collecting script',
   author = 'Peter Mikus',
   author_email = 'pmikus@cisco.com',
   url = 'https://github.com/pmikus/sys-info-suite',
-  download_url = 'https://github.com/pmikus/sys-info-suite/tarball/2.0.1',
+  download_url = 'https://github.com/pmikus/sys-info-suite/tarball/2.0.2',
   keywords = ['collecting', 'system', 'information', 'diff'],
   classifiers = [],
 )
